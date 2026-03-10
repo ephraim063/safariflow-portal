de57f385aafceb487653d559ffeed53099c48dcf
ephraim063 <ephraim063@gmail.com>
2026-03-10 11:50:36 +0300

    Add files via upload

--- safariflow-portal/src/App.jsx
+++ safariflow-portal/src/App.jsx
@@ -5,52 +5,27 @@ import Dashboard from './pages/Dashboard'
 import Quotes from './pages/Quotes'
 import NewQuote from './pages/NewQuote'
 import Clients from './pages/Clients'
+import Packages from './pages/Packages'
 import Settings from './pages/Settings'
+
+const Protected = ({ children }) => (
+  <>
+    <SignedIn><Layout>{children}</Layout></SignedIn>
+    <SignedOut><RedirectToSignIn /></SignedOut>
+  </>
+)
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={
-          <>
-            <SignedIn>
-              <Layout>
-                <Navigate to="/dashboard" replace />
-              </Layout>
-            </SignedIn>
-            <SignedOut><RedirectToSignIn /></SignedOut>
-          </>
-        } />
-        <Route path="/dashboard" element={
-          <>
-            <SignedIn><Layout><Dashboard /></Layout></SignedIn>
-            <SignedOut><RedirectToSignIn /></SignedOut>
-          </>
-        } />
-        <Route path="/quotes" element={
-          <>
-            <SignedIn><Layout><Quotes /></Layout></SignedIn>
-            <SignedOut><RedirectToSignIn /></SignedOut>
-          </>
-        } />
-        <Route path="/quotes/new" element={
-          <>
-            <SignedIn><Layout><NewQuote /></Layout></SignedIn>
-            <SignedOut><RedirectToSignIn /></SignedOut>
-          </>
-        } />
-        <Route path="/clients" element={
-          <>
-            <SignedIn><Layout><Clients /></Layout></SignedIn>
-            <SignedOut><RedirectToSignIn /></SignedOut>
-          </>
-        } />
-        <Route path="/settings" element={
-          <>
-            <SignedIn><Layout><Settings /></Layout></SignedIn>
-            <SignedOut><RedirectToSignIn /></SignedOut>
-          </>
-        } />
+        <Route path="/" element={<Protected><Navigate to="/dashboard" replace /></Protected>} />
+        <Route path="/dashboard" element={<Protected><Dashboard /></Protected>} />
+        <Route path="/packages" element={<Protected><Packages /></Protected>} />
+        <Route path="/quotes" element={<Protected><Quotes /></Protected>} />
+        <Route path="/quotes/new" element={<Protected><NewQuote /></Protected>} />
+        <Route path="/clients" element={<Protected><Clients /></Protected>} />
+        <Route path="/settings" element={<Protected><Settings /></Protected>} />
       </Routes>
     </BrowserRouter>
   )
